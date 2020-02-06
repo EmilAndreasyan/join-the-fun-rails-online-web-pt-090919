ced9f1609e45ae76a7b398a93a38c71f62c914e1
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-06 23:44:53 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "taxis", force: :cascade do |t|
-    t.integer  "ride_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
